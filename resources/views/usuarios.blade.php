7a7bd4fd593e0314b6c4404452f0ad247dc02add
Marcelo <chelo.salazar017@gmail.com>
2020-08-10 17:17:50 -0600

    Primer Commit

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">email</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($users as $user)
+        <tr>
+            <th scope="row">{{$user->id}}</th>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+        </tr>
+    @endforeach
+  </tbody>
+</table>
+
+@endsection
